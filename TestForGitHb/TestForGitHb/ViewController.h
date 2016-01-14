c3eedf8a1fd48d32415e1ee8edda702d6473cca5
yec8277 <yec8277@163.com>
2016-01-14 10:12:56 +0800

    第一次提交,版本1.0

---
+++ TestForGitHb/TestForGitHb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForGitHb
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 yec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestForGitHb/TestForGitHb/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestForGitHb
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 yec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
